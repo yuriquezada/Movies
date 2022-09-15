c9243ed98609622af45e7b02f9a8708c74061aae
yuriquezada <quezadat.yuri@gmail.com>
2022-09-14 21:36:10 -0500

    Initial commit

---
+++ app/src/main/java/com/yucode/movies/AspectRatioImageView.kt
@@ -0,0 +1,11 @@
+package com.yucode.movies
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class AspectRatioImageView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatImageView(context, attrs) {
+
+}
